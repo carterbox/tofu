1671f25aa7a57ae6297e859f1f7a5b6272bded5f
Daniel Ching <carterbox@users.noreply.github.com>
2022-10-16 00:19:51 -0500

    NEW: Show ComEd 5 minute price history

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:fl_chart/fl_chart.dart' as chart;
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -11,20 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'tofu',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Time OF Use'),
     );
   }
 }
@@ -32,84 +27,107 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+Future<EnergyRates> fetchRatesLast24Hours() async {
+  final response = await http.get(Uri.parse(
+      'https://hourlypricing.comed.com/api?type=5minutefeed&format=json'));
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return EnergyRates.fromJson(jsonDecode(response.body));
+    // return const EnergyRates(
+    //     period: 5, units: 'cents', rates: [1.0, 2.2, 3.3, 4.0]);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load rates from last 24 hours.');
+  }
+}
+
+class EnergyRates {
+  // period in minutes
+  final int period;
+  // The unit of measure per kwh
+  final String units;
+  final List<double> rates;
+
+  const EnergyRates({
+    required this.period,
+    required this.units,
+    required this.rates,
+  });
+
+  factory EnergyRates.fromJson(List<dynamic> json) {
+    return EnergyRates(
+      period: 5,
+      units: '\u{00A2}',
+      rates: json.map((x) => double.parse(x['price'])).toList(),
+    );
+  }
+}
+
+/// Return the pie char sections for the time of use
+List<chart.PieChartSectionData> timeOfUse(List<double> rates, int resample) {
+  List<double> smoothedRates = List<double>.filled(288 ~/ resample, 0.0);
+  for (int i = 0; i < rates.length; i++) {
+    smoothedRates[i ~/ resample] += rates[i] / resample;
+  }
+
+  return smoothedRates
+      .map(
+        (x) => chart.PieChartSectionData(
+          value: 1,
+          showTitle: true,
+          title: x.toStringAsFixed(1),
+          radius: x * 10,
+        ),
+      )
+      .toList();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Future<EnergyRates> futureRates;
+
+  @override
+  void initState() {
+    super.initState();
+    futureRates = fetchRatesLast24Hours();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: FutureBuilder<EnergyRates>(
+            future: futureRates,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return chart.PieChart(
+                  chart.PieChartData(
+                    sections: timeOfUse(snapshot.data!.rates, 6),
+                    centerSpaceRadius: double.infinity,
+                    startDegreeOffset: -90,
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                // TODO: Display a connection error message
+              }
+              return const CircularProgressIndicator();
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
