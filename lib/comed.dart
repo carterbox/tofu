318503c52503327cfcc3d6bfe7533831be4403a5
Daniel Ching <carterbox@users.noreply.github.com>
2022-10-19 21:56:50 -0500

    NEW: Add a license

--- lib/comed.dart
+++ lib/comed.dart
@@ -1,3 +1,19 @@
+// tofu, an app for visualizing time-of-use electricity rates
+// Copyright (C) 2022 Daniel Jackson Ching
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 /// Fetch electricity rate data from ComEd REST API.
 
 import 'dart:async';
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,19 @@
+// tofu, an app for visualizing time-of-use electricity rates
+// Copyright (C) 2022 Daniel Jackson Ching
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import 'dart:async';
 import 'dart:math';
 import 'package:fl_chart/fl_chart.dart' as chart;
@@ -63,11 +79,13 @@ List<chart.PieChartSectionData> timeOfUse(
 
 class _MyHomePageState extends State<MyHomePage> {
   late Future<EnergyRates> futureRates;
+  late Future<EnergyRates> pastRates;
 
   @override
   void initState() {
     super.initState();
-    futureRates = fetchRatesLast24Hours();
+    futureRates = fetchRatesNextDay();
+    pastRates = fetchRatesLastDay();
   }
 
   @override
